49b4546f7532b88121306f658149b46d0f5c3cae
Sourish Sarmah <sourishsarmah@gmail.com>
2022-01-26 23:25:40 +0530

    [Sourish] added error handling

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException
 from starlette.middleware.cors import CORSMiddleware
 
+from app.api.errors.error_handlers import (http422_error_handler,
+                                           http_error_handler)
 from app.api.routers.api import router
 from app.core.config import ALLOWED_HOSTS
 
@@ -13,5 +17,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.add_exception_handler(HTTPException, http_error_handler)
+app.add_exception_handler(RequestValidationError, http422_error_handler)
 
 app.include_router(router, prefix="/api")
